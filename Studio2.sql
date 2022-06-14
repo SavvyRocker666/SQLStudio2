362e74b11b3e9719cff9de56f0dc42220606bc40
Savvy <sabrinarocker16@gmail.com>
2022-06-13 20:21:04 -0500

    added cvs

--- Studio2.sql
+++ Studio2.sql
@@ -28,20 +28,4 @@ CREATE TABLE reference_books (
       ON UPDATE SET NULL
       ON DELETE SET NULL
 );
-INSERT INTO reference_books(edition, book_id)
-VALUE (5,32);
-CREATE TABLE genre (
-   genre_id INT PRIMARY KEY,
-   genres VARCHAR(100)
-);
-CREATE TABLE loan (
-   loan_id INT AUTO_INCREMENT PRIMARY KEY,
-   patron_id INT,
-   date_out DATE,
-   date_in DATE,
-   book_id INT,
-   FOREIGN KEY (book_id)
-      REFERENCES book(book_id)
-      ON UPDATE SET NULL
-      ON DELETE SET NULL
-);
+
